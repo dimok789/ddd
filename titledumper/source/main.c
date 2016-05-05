f4c5f4c665c77e9ab2b56fe46283b1aa5ad9da20
mcaboosec <mikah.chapman@gmail.com>
2016-05-05 00:03:21 -0600

    Cleanup after OS X fix

--- titledumper/source/main.c
+++ titledumper/source/main.c
@@ -27,6 +27,8 @@
 #include <inttypes.h>
 #ifndef __APPLE__
 #include <malloc.h>
+#define fopen64 fopen
+#define ftello64 ftello
 #endif
 #include <sys/stat.h>
 #include "Input.h"
@@ -183,9 +185,6 @@ void processTag(int client_socket, SendData *sendData)
         {
             lastTime = gettime();
             lastSize = 0;
-#ifdef __APPLE__
-#define fopen64 fopen
-#endif
             pFile = fopen64(localPath, "wb");
             printf("Open file: %s\n", localPath);
             if(!pFile) {
@@ -199,9 +198,6 @@ void processTag(int client_socket, SendData *sendData)
         if(!pFile) {
             break;
         }
-#ifdef __APPLE__
-#define ftello64 ftello
-#endif
         uint64_t size = (uint64_t)ftello64(pFile);
         unsigned int time = gettime();
         float fTimeDiff = (time - lastTime) * 0.001f;
